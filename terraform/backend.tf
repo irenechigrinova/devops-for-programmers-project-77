d99f9bb1a4f9183e33fb65f96bbe70b598d3a9ee
Irina Chigrinova <to.chigrinova@gmail.com>
2023-10-09 08:34:32 +0300

    backend file created

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-backend "s3" {
-    endpoint          = "storage.yandexcloud.net"
-    region            = "ru-central1"
-    key               = "yc-terraform-state-remote"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,6 +8,15 @@ terraform {
     #}
   }
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoint          = "storage.yandexcloud.net"
+    region            = "ru-central1"
+    key               = "yc-terraform-state-remote"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 provider "yandex" {
